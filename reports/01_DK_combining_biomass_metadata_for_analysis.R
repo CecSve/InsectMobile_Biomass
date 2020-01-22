4a5f349f7f202b9afc33723dc9020f31f065d458
Cecilie Svenningsen <cecilie.svenningsen@gmail.com>
2020-01-22 15:36:53 +0100

    Changed variable names to match to DE data for biomass measurements, habitat, timeband and route length

--- reports/01_DK_combining_biomass_metadata_for_analysis.R
+++ reports/01_DK_combining_biomass_metadata_for_analysis.R
@@ -7,21 +7,27 @@ library(ggplot2) # graphics
 library(digest) # applies a cryptographical hash function to arbitrary R objects
 library(forcats) # for working with categorical variables (factors)
 library(lubridate) # work with date and time formats
-library(here)
+#library(here)
 library(magrittr)
 
-# Prepare datasets for biomass data
+setwd("H:/Documents/Insektmobilen/Analysis/Biomass_test/January_2020")
+
+# Prepare datasets for biomass data - should be updated to this code when analysis has finished
+#biomass <-read.csv(here::here("raw-data/DK_Biomass_January_2020.csv"),header = TRUE,row.names = NULL,sep = ";") # Biomass data
+
 biomass <-
   read.csv(
-    here::here("raw-data/DK_Biomass_January_2020.csv"),
+    file = "Biomass_January_2020.csv",
     header = TRUE,
     row.names = NULL,
     sep = ";"
-  ) # Biomass data
+  )
 
 # Make the columns with biomass data into numeric values instead of factors
 cols = c(3:5)
-biomass[,cols] %<>% lapply(function(x) as.character(as.factor(x))) %>% lapply(function(x) as.numeric(as.character(x)))
+biomass[, cols] %<>% lapply(function(x)
+  as.character(as.factor(x))) %>% lapply(function(x)
+    as.numeric(as.character(x)))
 
 head(biomass)
 str(biomass)
@@ -36,21 +42,34 @@ str(biomass_nozeros)
 
 hist(log(biomass_nozeros$SampleBiomass_mg)) # Normally distributed but we still need to log transform due to a surplus of low weights
 
-# Import metadata
+# Import metadata - change to here::here when analysis is ready to be published
 samplingevent <-
   read.csv(
-    here::here("raw-data/DK_SamplingEvent.csv"),
+    file = "SamplingEvent.csv",
     header = TRUE,
     row.names = NULL,
     sep = ";",
     check.names = FALSE,
     na.strings = c("", " ", "NA")
   )
+
 meta <- samplingevent %>% filter(FullySampled == 'yes') # Filter function to only select the samples that are correctly sampled
-meta_select <-  select(meta, 'SampleID', 'LandUSeType', 'RouteURL', 'NewRouteURL', 'Date', 'StartTime', 'EndTime', 'Wind', 'Temperature') %>% droplevels # We choose only the variables we're interested in
+meta_select <-
+  select(
+    meta,
+    'SampleID',
+    'LandUSeType',
+    'RouteURL',
+    'NewRouteURL',
+    'Date',
+    'StartTime',
+    'EndTime',
+    'Wind',
+    'Temperature'
+  ) %>% droplevels # We choose only the variables we're interested in
 
 summary(meta_select$LandUSeType) # Look at the different types of land use categories and how many samples are present in each category
-metadata2 <- subset(meta_select, !is.na(meta_select$LandUSeType)) # subset your data further and removes samples that does not have a landuse type
+metadata2 <- subset(meta_select,!is.na(meta_select$LandUSeType)) # subset your data further and removes samples that does not have a landuse type
 
 # Rename the values from Danish to English
 metadata2$LandUSeType <- mapvalues(
@@ -89,7 +108,13 @@ data <- inner_join(metadata2, biomass_nozeros, by = "SampleID")  # Join the two 
 head(data)
 
 # choose only the clean land use types
-data_landuse <-  data %>% filter(LandUSeType == 'agriculture' | LandUSeType == 'forest' | LandUSeType == 'dry' | LandUSeType == 'urban'| LandUSeType =='wet') %>% droplevels
+data_landuse <-
+  data %>% filter(
+    LandUSeType == 'agriculture' |
+      LandUSeType == 'forest' |
+      LandUSeType == 'dry' |
+      LandUSeType == 'urban' | LandUSeType == 'wet'
+  ) %>% droplevels
 
 # get summaries of how many samples there is for each variable and their levels
 data_landuse %>% group_by(LandUSeType) %>% summarize(count=n()) # count how many samples there is from each coarse land-use category
@@ -99,4 +124,21 @@ data.frame(table(data_landuse$Wind)) # how often were the different wind categor
 data.frame(table(data_landuse$Temperature)) # how many samples were collected at different temperature intervals
 data.frame(table(data_landuse$Date)) # how many samples per day
 
-write.table(data_landuse, file = "cleaned-data/DK_rough_landuse_biomass.txt")
+# change column header to macth DE variable names and drop google maps route links
+data <-
+  data_landuse %>% select(-RouteURL,-NewRouteURL) %>% rename(
+    Habitat = LandUSeType,
+    Biomass_small = DryMassSmall_mg,
+    Biomass_large = DryMassLarge_mg,
+    Biomass = SampleBiomass_mg
+  )
+
+# make a column for whether sampling was midday or evening
+time <- as.POSIXct(strptime(data$StartTime,"%H:%M"),"UTC")
+x=as.POSIXct(strptime(c("120000", "150000", "170000", "195858"), "%H%M%S"), "UTC")
+data$TimeBand <- case_when(between(time, x[1], x[2]) ~"midday", between(time, x[3], x[4])~"evening")
+
+# adding a column for route length
+data$Route_length <- '5'
+
+write.table(data, file = "DK_rough_landuse_biomass_editedheaders.txt")

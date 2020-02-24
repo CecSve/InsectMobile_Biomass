5299dbccfc87528277b9f3d7a0f0b764962fd1bb
Bowler <db40fysa@idiv.de>
2020-02-24 16:12:16 +0100

    Work on gls model

--- reports/04_landuse_categories.R
+++ reports/04_landuse_categories.R
@@ -5,8 +5,14 @@ library(ggplot2)
 library(plyr)
 library(ggpirate)
 library(cowplot)
+library(wesanderson)
 
-#Fig 2
+####################################################################
+
+#decide on common color scheme
+landuseCols <- wes_palette('Darjeeling1', 5, type = c("discrete"))
+
+###Fig 2############################################################
 
 #total biomass
 
@@ -22,22 +28,6 @@ ggplot(allInsects,aes(x=Land_use, y=log(Biomass+1)))+
   facet_wrap(~Country)+
   xlab("Land cover")+ylab("Total insect biomass")
 
-
-#split by size
-g1 <- ggplot(allInsects,aes(x=Land_use, y=log(Biomass_large+1)))+
-  geom_boxplot(aes(colour=Time_band,fill=Time_band),show.legend=TRUE)+
-  theme_bw()+
-  facet_wrap(~Country)+
-  xlab("Land cover")+ylab("Large insect biomass")
-
-g2 <- ggplot(allInsects,aes(x=Land_use, y=log(Biomass_small+1)))+
-  geom_boxplot(aes(colour=Time_band,fill=Time_band),show.legend=TRUE)+
-  theme_bw()+
-  facet_wrap(~Country)+
-  xlab("Land cover")+ylab("Small insect biomass")
-
-plot_grid(g1,g2)
-
 #ignoring time-band
 ggplot(allInsects,aes(x=Land_use, y=log10(Biomass+1)))+
   geom_boxplot(fill="red")+
@@ -45,55 +35,54 @@ ggplot(allInsects,aes(x=Land_use, y=log10(Biomass+1)))+
   facet_wrap(~Country)+
   xlab("Land cover")+ylab("Insect biomass")
 
-temp <- ddply(allInsects,.(Land_use,Country),summarise,
-              meanInsects=mean(log10(Biomass+1)),
-              sdInects=sd(log(Biomass+1)))
-
-ggplot(temp,aes(x=Land_use, y=meanInsects))+
-  geom_bar(fill="red",stat="identity")+
+#split by size
+g1 <- ggplot(allInsects,aes(x=Land_use, y=log(Biomass_large+1),colour=Land_use))+
+  geom_boxplot(fill="white")+
+  scale_color_manual(values=landuseCols)+
   theme_bw()+
   facet_wrap(~Country)+
-  xlab("Land cover")+ylab("Insect biomass")
-
-g1 <- ggplot(allInsects,aes(x=Land_use, y=log(Biomass_large+1)))+
-  geom_boxplot(colour="darkblue",fill="white")+
-  theme_bw()+
-  facet_wrap(~Country)+
+  theme(legend.position = "none")+
   xlab("Land cover")+ylab("Large insect biomass")
 
-g2 <- ggplot(allInsects,aes(x=Land_use, y=log(Biomass_small+1)))+
-  geom_boxplot(colour="darkblue",fill="white")+
+g2 <- ggplot(allInsects,aes(x=Land_use, y=log(Biomass_small+1),colour=Land_use))+
+  geom_boxplot(fill="white")+
+  scale_color_manual(values=landuseCols)+
   theme_bw()+
   facet_wrap(~Country)+
+  theme(legend.position = "none")+
   xlab("Land cover")+ylab("Small insect biomass")
 
 plot_grid(g1,g2,ncol=1)
 
-#relationship between small and large biomass
-ggplot(allInsects)+
-  geom_point(aes(x=log(Biomass_small+1),y=log(Biomass_large+1),colour=Land_use))+
-  facet_wrap(Time_band~Country)+
-  geom_abline(intercept=0,slope=1)
+###lm##############################################################
 
+hist(log(allInsects$Biomass+1))
 
-###quick analysis
-lm1 <- lm(log(Biomass+1) ~ Land_use + Time_band + Country, data=allInsects)
+lm1 <- lm(log(Biomass+1) ~ Land_use + Time_band + StartTime + , data=allInsects)
 summary(lm1)
 
 
-#more detailed analysis - with random effects
-library(lme4)
-lme1 <- lmer(log(Biomass+1) ~ Land_use + Time_band + Country +
-               Country:StartTime + Country:yDay + 
-               (1|RouteID) + (1|PilotID) + offset(log(Distance_driven)),
-             data=insectsDE)
+#plus random effects##############################################################
 
-#spatial model
+library(lme4)
+library(lmerTest)
+lme1 <- lmer(log(Biomass+1) ~ Land_use + Time_band + 
+               Time_band:as.numeric(StartTime) + yDay + 
+               (1|RouteID) + (1|PilotID), data=allInsects)
+
+summary(lme1)
+
+#plus spatial models################################################################
+
 library(nlme)
-gls1 <- lme(log(Biomass+1) ~ Land_use + Time_band + Country +
-              Country:StartTime + Country:yDay,
-            offset=log(Distance_driven),
-            random=~1|PilotID/RouteID,
-            correlation=corExp(form=~x+y),
-            data=insectsDE)
+
+#jitter x and y slightly - fix leter
+allInsects$x2 <- allInsects$x + rnorm(length(allInsects$x),0,100)
+allInsects$y2 <- allInsects$y + rnorm(length(allInsects$y),0,100)
+
+gls1 <- lme(log(Biomass+1) ~ Land_use + Time_band + 
+              Time_band:as.numeric(StartTime) + yDay,
+            random=~1|RouteID,
+            correlation=corExp(form=~x2+y2|RouteID),
+            data=allInsects,na.action=na.omit)
 

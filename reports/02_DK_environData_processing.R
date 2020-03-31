aa5512373e41f4bd6fc75957f8b38d686bd2faf7
Cecilie Svenningsen <cecilie.svenningsen@gmail.com>
2020-03-31 16:18:29 +0200

    changed script to only process and combine land use data with already processed metadata. Output created in cleaned-data as input for subsequent spatial analysis

--- reports/02_DK_environData_processing.R
+++ reports/02_DK_environData_processing.R
@@ -2,57 +2,60 @@
 
 library(tidyverse)
 library(readr)
-library(ggplot2)
 
 # load buffer zone files 
 oeko <- read.delim("covariate-data/DK_ruter2018_OekoAreas.txt")
 hedge <- read.delim("covariate-data/DK_ruter2018_hegnAreas.txt")
-'50m' <- read_delim("covariate-data/DK_ruter2018buf50_areas.txt","\t", escape_double = FALSE, trim_ws = TRUE)
-'250m' <- read_delim("covariate-data/DK_ruter2018buf250_areas.txt","\t", escape_double = FALSE, trim_ws = TRUE)
-'500m' <- read_delim("covariate-data/DK_ruter2018buf500_areas.txt","\t", escape_double = FALSE, trim_ws = TRUE)
-'1000m' <- read_delim("covariate-data/DK_ruter2018buf1000_areas.txt","\t", escape_double = FALSE, trim_ws = TRUE)
+buf_50m <- read_delim("covariate-data/DK_ruter2018buf50_areas.txt","\t", escape_double = FALSE, trim_ws = TRUE)
+buf_250m <- read_delim("covariate-data/DK_ruter2018buf250_areas.txt","\t", escape_double = FALSE, trim_ws = TRUE)
+buf_500m <- read_delim("covariate-data/DK_ruter2018buf500_areas.txt","\t", escape_double = FALSE, trim_ws = TRUE)
+buf_1000m <- read_delim("covariate-data/DK_ruter2018buf1000_areas.txt","\t", escape_double = FALSE, trim_ws = TRUE)
 
 # load pilotripids and routeids (prepared by Jesper)
-tripids <- read.delim("cleaned-data/DK_pilotTripIdToRouteID.txt", sep = ";")
+tripids <- read.delim("cleaned-data/DK_pilotTripIdToRouteID.txt")
 
 # load metadata
 metadata <- read.delim("cleaned-data/DK_rough_landuse_biomass.txt", sep = " ")
 
 # load centroid coordinates for each route
-coords <- read.delim("covariate-data/DK_ruter2018_pkt_koordinater.txt", sep = ";")
+coords <- read.delim("covariate-data/DK_ruter2018_pkt_koordinater.txt")
 
 # load traffic light counts per route
 stops <- read.delim("covariate-data/DK_TrafficLightsCount.txt")
 
-# merging data
+# merging data by new routeIDs (RouteID_JB) so other data can be merged as well
 mergedData <- merge(metadata, tripids, by.x= "SampleID", by.y= "PilotTripID")
-setdiff(metadata$SampleID, tripids$PilotTripID) # we lose the sample P61.1B. This is beacause no route has been sent to Jesper even though the sample has been processed in lab
+setdiff(metadata$SampleID, tripids$PilotTripID) # all metadatasamples are included - yay!
 
-# examining stopping effect (proxy = count of traffic lights on route) on biomass
+# adding stopping effect (proxy = count of traffic lights on route) 
 with_stops <- merge(mergedData, stops, by.x = "RouteID_JB", by.y = "routeID", all = T)
-  
-# not facetted by landuse type
-with_stops %>% mutate(Biomass = replace(Biomass,is.na(Biomass),0)) %>% ggplot(aes(with_stops))+
-  geom_point(aes(x=log(Num_trafficLights+1),y=log(Biomass+1)))+
-  #facet_wrap(~Land_use)+
-  stat_smooth(method="lm",aes(x=log(Num_trafficLights+1),y=log(Biomass+1)))
 
-# by landuse
-with_stops %>% mutate(Biomass = replace(Biomass,is.na(Biomass),0)) %>% ggplot(aes(with_stops))+
-  geom_point(aes(x=log(Num_trafficLights+1),y=log(Biomass+1)))+
-  facet_wrap(~Land_use)+
-  stat_smooth(method="lm",aes(x=log(Num_trafficLights+1),y=log(Biomass+1)))
+# removing samples from stops that don't have biomass
+mergedData <- with_stops %>% drop_na(SampleID)
+mergedData <- mergedData %>% mutate(Num_trafficLights = replace(Num_trafficLights,is.na(Num_trafficLights),0)) # recode NAs to zeros for number of traffic ligths on the routes
 
-# by landuse - small size fraction
-with_stops %>% mutate(Biomass_small = replace(Biomass_small,is.na(Biomass_small),0)) %>% ggplot(aes(with_stops))+
-  geom_point(aes(x=log(Num_trafficLights+1),y=log(Biomass_small+1)))+
-  facet_wrap(~Land_use)+
-  stat_smooth(method="lm",aes(x=log(Num_trafficLights+1),y=log(Biomass_small+1)))
+# adding utm coordinates for route centroids
+mergedData <- merge(mergedData, coords, by.x= "RouteID_JB", by.y= "routeID")
+mergedData <- select(mergedData, -OBJECTID) # remove objectid column since it is not needed  
 
-# by landuse - large size fraction
-with_stops %>% mutate(Biomass_large = replace(Biomass_large,is.na(Biomass_large),0)) %>% ggplot(aes(with_stops))+
-  geom_point(aes(x=log(Num_trafficLights+1),y=log(Biomass_large+1)))+
-  facet_wrap(~Land_use)+
-  stat_smooth(method="lm",aes(x=log(Num_trafficLights+1),y=log(Biomass_large+1)))
+write.table(mergedData, file = "cleaned-data/DK_mergedData.txt") # save updated metadata that now contains number of traffic lights on routes and centroid coordinates for each route
 
+# merge buffer data with mergeddata
+hedgeData <- merge(mergedData, hedge, by.x= "RouteID_JB", by.y= "routeID")
+oekoData <- oeko %>% rename(RouteID_JB = routeID) %>% inner_join(mergedData, oeko, by = c("RouteID_JB"))
 
+# save output
+write.table(hedgeData, file = "cleaned-data/DK_hedgeData.txt")
+write.table(oekoData, file = "cleaned-data/DK_oekoData.txt")
+
+# create a dataframe where each buffer category and mergeData are combined
+data_50m <- buf_50m %>% rename(RouteID_JB = routeID) %>% inner_join(mergedData, buf_50m, by = c("RouteID_JB"))
+data_250m <- buf_250m %>% rename(RouteID_JB = routeID) %>% inner_join(mergedData, buf_250m, by = c("RouteID_JB"))
+data_500m <- buf_500m %>% rename(RouteID_JB = routeID) %>% inner_join(mergedData, buf_500m, by = c("RouteID_JB"))
+data_1000m <- buf_1000m %>% rename(RouteID_JB = routeID) %>% inner_join(mergedData, buf_1000m, by = c("RouteID_JB"))
+
+# save output
+write.table(data_50m, file = "cleaned-data/DK_landusedata_50m.txt")
+write.table(data_250m, file = "cleaned-data/DK_landusedata_250m.txt")
+write.table(data_500m, file = "cleaned-data/DK_landusedata_500m.txt")
+write.table(data_1000m, file = "cleaned-data/DK_landusedata_1000m.txt")

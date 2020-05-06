b3370f3a6e0aacc3b5ba90a4bf928080b3ca11e6
Diana Bowler <diana.bowler@idiv.de>
2020-05-06 12:11:34 +0200

    merging weather, land use, stop and insect data for DE

--- reports/03_mergeData.R
+++ reports/03_mergeData.R
@@ -41,9 +41,42 @@ tempData <- read.delim("cleaned-data/routeTemps_DE.txt",as.is=T)
 stopData <- read.delim("cleaned-data/stops_DE.txt",as.is=T)
 tlData <- read.delim("cleaned-data/trafficlights_DE.txt",as.is=T)
 
-#merge
+#merge weather
 windData <- melt(windData[,-1],id=c("RouteID","Date"))
 names(windData)[which(names(windData)=="value")] <- "Wind"
-names(windData)[which(names(windData)=="varible")] <- "Time_band"
+names(windData)[which(names(windData)=="variable")] <- "Time_band"
+windData$Time_band <- as.character(windData$Time_band)
+windData$Time_band[which(windData$Time_band=="middayTemp")] <- "midday"
+windData$Time_band[which(windData$Time_band=="eveningTemp")] <- "evening"
+
+tempData <- melt(tempData[,-1],id=c("RouteID","Date"))
+names(tempData)[which(names(tempData)=="value")] <- "Temp"
+names(tempData)[which(names(tempData)=="variable")] <- "Time_band"
+tempData$Time_band <- as.character(tempData$Time_band)
+tempData$Time_band[which(tempData$Time_band=="middayTemp")] <- "midday"
+tempData$Time_band[which(tempData$Time_band=="eveningTemp")] <- "evening"
+
+weatherData <- merge(tempData,windData,by=c("RouteID","Date","Time_band"))
+allInsects <- allInsects[,-which(names(allInsects) %in% c("Wind","Temperature"))]
+allInsects2 <- merge(allInsects,weatherData,by=c("RouteID","Date","Time_band"),all.x=T)
+subset(allInsects2,is.na(Temp))
+#Gruen_09 2018-07-04 is missing - but I did this!!! Grr
+
+#insert mean values for now
+mean(allInsects2$Temp[allInsects$Time_band=="midday"],na.rm=T)#24.42077
+mean(allInsects2$Wind[allInsects$Time_band=="midday"],na.rm=T)#4.163846
+mean(allInsects2$Temp[allInsects$Time_band=="evening"],na.rm=T)#23.01231
+mean(allInsects2$Wind[allInsects$Time_band=="evening"],na.rm=T)#3.693846
+allInsects2$Temp[is.na(allInsects2$Temp)] <- c(24.42077,23.01231)
+allInsects2$Wind[is.na(allInsects2$Temp)] <- c(4.163846,3.693846)
+allInsects <- allInsects
+
+#land use data
+allInsects <- merge(allInsects,environData,by.x="RouteID",by.y="Codierung",all.x=T)
+
+#stop data
+stopData <- merge(tlData,stopData,by="Codierung",all=T)
+names(stopData)[which(names(stopData)=="osm")] <- "stops"
+allInsects <- merge(allInsects,stopData,by.x="RouteID",by.y="Codierung",all.x=T)
 
 ####Denmark??

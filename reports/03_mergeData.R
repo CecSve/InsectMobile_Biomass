d8b8f54f281e381ba46c5fdba04c9770e3d3e10e
Diana Bowler <diana.bowler@idiv.de>
2020-05-06 11:16:14 +0200

    merging weather data

--- reports/03_mergeData.R
+++ reports/03_mergeData.R
@@ -7,18 +7,24 @@ insectsDK$Country <- "Denmark"
 insectsDE <- read.delim("cleaned-data/DE_rough_landuse_biomass.txt")
 insectsDE$Country <- "Germany"
 
-#common columns
-myVars <- c("Land_use","Time_band","Country",
-            "Biomass","Biomass_small","Biomass_large",
-            "Date")
+# #common columns
+# myVars <- c("Land_use","Time_band","Country",
+#             "Biomass","Biomass_small","Biomass_large",
+#             "Date")
+# 
+# 
+# allInsects <- rbind(insectsDK[,myVars],
+#                     insectsDE[,myVars])
 
 
-allInsects <- rbind(insectsDK[,myVars],
-                    insectsDE[,myVars])
+####merge all german files ###########################################################
 
+allInsects <- insectsDE
+
+library(lubridate)
+library(reshape2)
 
 #format Date
-library(lubridate)
 allInsects$Date <- as.Date(allInsects$Date)
 allInsects$yDay <- yday(allInsects$Date)
 
@@ -28,3 +34,14 @@ allInsects$Time_band <- factor(allInsects$Time_band,levels=c("midday","evening")
 allInsects$Land_use <- factor(allInsects$Land_use,levels=c("Urban","Farmland",
                                                            "Dryland","Wetland","Forest"))
 
+#add on environmental data
+environData <- read.delim("cleaned-data/environData_DE.txt",as.is=T)
+windData <- read.delim("cleaned-data/routeWind_DE.txt",as.is=T)
+tempData <- read.delim("cleaned-data/routeTemps_DE.txt",as.is=T)
+stopData <- read.delim("cleaned-data/stops_DE.txt",as.is=T)
+tlData <- read.delim("cleaned-data/trafficlights_DE.txt",as.is=T)
+
+#merge
+windData <- melt(windData[,-1],id=c("RouteID","Date"))
+names(windData)[which(names(windData)=="value")] <- "Wind"
+names(windData)[which(names(windData)=="varible")] <- "Time_band"

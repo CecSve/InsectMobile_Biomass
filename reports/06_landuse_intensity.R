b2de30290015890895b2eca4436bdffd0a5d381e
Diana Bowler <diana.bowler@idiv.de>
2020-05-13 12:53:43 +0200

    Plot farmland and urban intensity variables for DE

--- reports/06_landuse_intensity.R
+++ reports/06_landuse_intensity.R
@@ -6,19 +6,52 @@ library(wesanderson)
 library(ggpubr)
 library(scales)
 
-####colour################################################################
+### DE urban##############################################
 
-#decide on common color scheme
-landuseCols <- wes_palette('Darjeeling1', 5, type = c("discrete"))
+g1 <- ggplot(subset(allInsects,Land_use=="Urban"),
+       aes(x=sqrt(urbangreen_1000),y=(Biomass+1)))+
+  geom_point(col=landuseCols[1])+
+  scale_y_log10() +
+  theme_bw() +
+  geom_smooth(method="lm",color="grey70")+
+  xlab("Urban green space cover") +ylab("Biomass")
 
-### DE plot land cover##############################################
+g2 <- ggplot(subset(allInsects,Land_use=="Urban"),
+             aes(x=sqrt(roads_1000),y=(Biomass+1)))+
+  geom_point(col=landuseCols[1])+
+  scale_y_log10() +
+  theme_bw() +
+  geom_smooth(method="lm",color="grey70")+
+  xlab("Road density") +ylab("Biomass")
 
-qU <- ggplot(allInsects)+
-              geom_point(aes(x=Roads_1000,y=(Biomass+1)),
-                         col=landuseCols[1])+
-              scale_y_log10() +
-              theme_bw() +
-              xlab("Road density") +ylab("Biomass")
+plot_grid(g1,g2)
 
+### DE farmland##########################################
+
+g1 <- ggplot(subset(allInsects,Land_use=="Farmland"),
+             aes(x=sqrt(hedges_1000),y=(Biomass+1)))+
+  geom_point(col=landuseCols[1])+
+  scale_y_log10() +
+  theme_bw() +
+  geom_smooth(method="lm",color="grey70")+
+  xlab("Hedges") +ylab("Biomass")
+
+g2 <- ggplot(subset(allInsects,Land_use=="Farmland"),
+             aes(x=exAgr_1000,y=(Biomass+1)))+
+  geom_point(col=landuseCols[1])+
+  scale_y_log10() +
+  theme_bw() +
+  geom_smooth(method="lm",color="grey70")+
+  xlab("Extensive farmland") +ylab("Biomass")
+
+g3 <- ggplot(subset(allInsects,Land_use=="Farmland"),
+             aes(x=intAgr_1000,y=(Biomass+1)))+
+  geom_point(col=landuseCols[1])+
+  scale_y_log10() +
+  theme_bw() +
+  geom_smooth(method="lm",color="grey70")+
+  xlab("Intensive farmland") +ylab("Biomass")
+
+plot_grid(g1,g2,g3,nrow=1)
 
 

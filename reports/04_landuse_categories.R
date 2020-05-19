a0364c1faea9db1daaeb595a2f88497d45821d87
Diana Bowler <diana.bowler@idiv.de>
2020-05-19 15:48:34 +0200

    fix merge error

--- reports/04_landuse_categories.R
+++ reports/04_landuse_categories.R
@@ -12,8 +12,9 @@ library(wesanderson)
 #decide on common color scheme
 landuseCols <- wes_palette('Darjeeling1', 5, type = c("discrete"))
 landuseCols <- landuseCols[c(1,4,3,5,2)]
-landuseOrder <- c("Urban","Farmland","Open uncultivated","Wetland","Forest")
 
+landuseOrder <- c("Urban","Farmland","Open uncultivated","Wetland","Forest")
+landuseOrderDK <- c("Urban","Farmland","Open uncultivated land","Wetland","Forest")
 
 ###Fig 2############################################################
 
@@ -63,6 +64,15 @@ g2 <- ggplot(allInsects,aes(x=Land_use, y=log(Biomass_small+1),colour=Land_use))
   facet_wrap(~Country)+
   theme(legend.position = "none")+
   xlab("Land cover")+ylab("Small insect biomass")
+
+#add total biomass and correct colours and labels
+g3 <- ggplot(allInsects,aes(x=Land_use, y=(Biomass),colour=Land_use))+
+  geom_boxplot(fill="white", lwd = 1.2)+
+  scale_color_manual(values=landuseCols)+
+  scale_y_log10() +
+  theme_bw(base_size = 15)+
+  theme(legend.position = "none")+
+  xlab("Land cover")+ylab("Insect biomass (log-transformed)") + scale_x_discrete(labels=c("Agriculture" = "Farmland"))
 
 plot_grid(g1,g2,ncol=1)
 
@@ -218,16 +228,32 @@ r.squaredGLMM(gls1)
 
 #final model DK - use cStops instead of cTL
 gls1 <- lme(log(Biomass+1) ~ Land_use + Time_band + 
-              Time_band:cnumberTime + cStops,
+              Time_band:cnumberTime + cyDay + cStops,
             random=~1|PilotID/RouteID_JB,
             correlation=corExp(form=~x2+y2|PilotID/RouteID_JB),
             data=allInsects,na.action=na.omit)
 summary(gls1)
+
+# set open uncultivated land as reference instead of urban to get summry statistics for urban
+test <- within(allInsects, Land_use <- relevel(Land_use, ref = "Wetland"))
+gls1 <- lme(log(Biomass) ~ Land_use + Time_band + 
+              Time_band:cnumberTime + cyDay + cStops,
+            random=~1|PilotID/RouteID_JB,
+            correlation=corExp(form=~x2+y2|PilotID/RouteID_JB),
+            data=test,na.action=na.omit)
+summary(gls1)
+
 #keep in TL even if not significant
-
 r.squaredGLMM(gls1)
-
 # Summary DK
 #R2m       R2c
 #[1,] 0.2686265 0.6022614
-# ? interpretation???
+
+library(lsmeans)
+emm1 <- emmeans(gls1, specs = pairwise ~ Land_use, type = "response") # get results on untransformed response
+emm1$emmeans
+emm1$contrasts
+emm1$contrasts %>% summary(infer = TRUE)
+plot(emm1, comparisons = TRUE) + theme_bw() + 
+  labs(x = "Estimated marginal mean (biomass)", y = "Land use category") + scale_y_discrete(labels=c("Agriculture" = "Farmland")) # The blue bars are confidence intervals for the EMMs, and the red arrows are for the comparisons among them. If an arrow from one mean overlaps an arrow from another group, the difference is not significant.
+

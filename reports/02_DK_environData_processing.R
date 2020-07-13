7fc3e0d06dc237a9d394f9536250551e61574809
Cecilie Svenningsen <cecilie.svenningsen@gmail.com>
2020-07-13 12:01:50 +0200

    Added new DK data and had to update script -have not included the 500 m buffer with NEP (net ecosystem productivity) yet

--- reports/02_DK_environData_processing.R
+++ reports/02_DK_environData_processing.R
@@ -9,20 +9,28 @@ library(ggpubr)
 library(tidyr)
 
 # load buffer zone files 
-oeko <- read.delim("covariate-data/DK_ruter2018_OekoAreas.txt")
+#oeko <- read.delim("covariate-data/DK_ruter2018_OekoAreas.txt") # oeko is now part of the buffer zone data
 hedge <- read.delim("covariate-data/DK_ruter2018_hegnAreas.txt")
 urbangreen <- read.delim("covariate-data/DK_ruter2018_urbGreenAreas.txt")
-buf_50m <- read_delim("covariate-data/DK_ruter2018buf50_areas.txt","\t", escape_double = FALSE, trim_ws = TRUE)
-buf_250m <- read_delim("covariate-data/DK_ruter2018buf250_areas.txt","\t", escape_double = FALSE, trim_ws = TRUE)
-buf_500m <- read_delim("covariate-data/DK_ruter2018buf500_areas.txt","\t", escape_double = FALSE, trim_ws = TRUE)
-buf_1000m <- read_delim("covariate-data/DK_ruter2018buf1000_areas.txt","\t", escape_double = FALSE, trim_ws = TRUE)
+nep <- read_delim("covariate-data/ruter2018buf500_NEP.txt","\t", escape_double = FALSE, trim_ws = TRUE)
+buf_50m <- read_delim("covariate-data/ruter2018buf50_areas.txt","\t", escape_double = FALSE, trim_ws = TRUE)
+buf_250m <- read_delim("covariate-data/ruter2018buf250_areas.txt","\t", escape_double = FALSE, trim_ws = TRUE)
+buf_500m <- read_delim("covariate-data/ruter2018buf500_areas.txt","\t", escape_double = FALSE, trim_ws = TRUE)
+buf_1000m <- read_delim("covariate-data/ruter2018buf1000_areas.txt","\t", escape_double = FALSE, trim_ws = TRUE)
 
 ##### Reformatting environmental data to WIDE format with tidyr #######
 # transform oekodata from long to wide format prior to merging 
-oekocast <-
-  oeko %>% pivot_wider(names_from = bufferDist,
-                       values_from = propOeko,
-                       names_prefix = "propOeko_")
+#oekocast <- oeko %>% pivot_wider(names_from = bufferDist, values_from = propOeko, names_prefix = "propOeko_")
+
+# rename the land use categories to _organic if it is registered as organic
+setDT(buf_50m)
+buf_50m[oeko == "1", type := paste0(type, "_", "organic")]
+setDT(buf_250m)
+buf_250m[oeko == "1", type := paste0(type, "_", "organic")]
+setDT(buf_500m)
+buf_500m[oeko == "1", type := paste0(type, "_", "organic")]
+setDT(buf_1000m)
+buf_1000m[oeko == "1", type := paste0(type, "_", "organic")]
 
 #test <- dcast(melt(oeko, id.vars=c("routeID", "bufferDist", "propOeko")), routeID~bufferDist+propOeko)
 
@@ -52,22 +60,22 @@ buf_500m$propLand_use <- NA
 buf_1000m$propLand_use <- NA
 
 #wetland
-buf_50m$propLand_use[buf_50m$type %in% c("Sø", "Strandeng", "Mose")] <- "Wetland"
-buf_250m$propLand_use[buf_250m$type %in% c("Sø", "Strandeng", "Mose")] <- "Wetland"
-buf_500m$propLand_use[buf_500m$type %in% c("Sø", "Strandeng", "Mose")] <- "Wetland"
-buf_1000m$propLand_use[buf_1000m$type %in% c("Sø", "Strandeng", "Mose")] <- "Wetland"
+buf_50m$propLand_use[buf_50m$type %in% c("Sø", "Mose", "Sø_organic", "Mose_organic")] <- "Wetland"
+buf_250m$propLand_use[buf_250m$type %in% c("Sø", "Mose", "Sø_organic", "Mose_organic")] <- "Wetland"
+buf_500m$propLand_use[buf_500m$type %in% c("Sø", "Mose", "Sø_organic", "Mose_organic")] <- "Wetland"
+buf_1000m$propLand_use[buf_1000m$type %in% c("Sø", "Mose", "Sø_organic", "Mose_organic")] <- "Wetland"
 
 #forest
-buf_50m$propLand_use[buf_50m$type %in% "Skov"] <- "Forest"
-buf_250m$propLand_use[buf_250m$type %in% "Skov"] <- "Forest"
-buf_500m$propLand_use[buf_500m$type %in% "Skov"] <- "Forest"
-buf_1000m$propLand_use[buf_1000m$type %in% "Skov"] <- "Forest"
+buf_50m$propLand_use[buf_50m$type %in% c("Skov","Skov_organic")] <- "Forest"
+buf_250m$propLand_use[buf_250m$type %in% c("Skov","Skov_organic")] <- "Forest"
+buf_500m$propLand_use[buf_500m$type %in% c("Skov","Skov_organic")] <- "Forest"
+buf_1000m$propLand_use[buf_1000m$type %in% c("Skov","Skov_organic")] <- "Forest"
 
 #agriculture
-buf_50m$propLand_use[buf_50m$type %in% c("Ekstensiv", "Intensiv", "Markblok", "Semi-intensiv")] <- "Agriculture"
-buf_250m$propLand_use[buf_250m$type %in% c("Ekstensiv", "Intensiv", "Markblok", "Semi-intensiv")] <- "Agriculture"
-buf_500m$propLand_use[buf_500m$type %in% c("Ekstensiv", "Intensiv", "Markblok", "Semi-intensiv")] <- "Agriculture"
-buf_1000m$propLand_use[buf_1000m$type %in% c("Ekstensiv", "Intensiv", "Markblok", "Semi-intensiv")] <- "Agriculture"
+buf_50m$propLand_use[buf_50m$type %in% c("Ekstensiv", "Ekstensiv_organic", "Intensiv", "Intensiv_organic", "Markblok", "Markblok_organic", "Semi-intensiv", "Semi-intensiv_organic")] <- "Agriculture"
+buf_250m$propLand_use[buf_250m$type %in% c("Ekstensiv", "Ekstensiv_organic", "Intensiv", "Intensiv_organic", "Markblok", "Markblok_organic", "Semi-intensiv", "Semi-intensiv_organic")] <- "Agriculture"
+buf_500m$propLand_use[buf_500m$type %in% c("Ekstensiv", "Ekstensiv_organic", "Intensiv", "Intensiv_organic", "Markblok", "Markblok_organic", "Semi-intensiv", "Semi-intensiv_organic")] <- "Agriculture"
+buf_1000m$propLand_use[buf_1000m$type %in% c("Ekstensiv", "Ekstensiv_organic", "Intensiv", "Intensiv_organic", "Markblok", "Markblok_organic", "Semi-intensiv", "Semi-intensiv_organic")] <- "Agriculture"
 
 #urban
 buf_50m$propLand_use[buf_50m$type %in% c("Lav bebyggelse", "Erhverv", "Høj bebyggelse", "Bykerne")] <- "Urban"
@@ -76,10 +84,10 @@ buf_500m$propLand_use[buf_500m$type %in% c("Lav bebyggelse", "Erhverv", "Høj be
 buf_1000m$propLand_use[buf_1000m$type %in% c("Lav bebyggelse", "Erhverv", "Høj bebyggelse", "Bykerne")] <- "Urban"
 
 #open uncultivated land
-buf_50m$propLand_use[buf_50m$type %in% c("Hede", "Overdrev", "Eng")] <- "Open uncultivated land"
-buf_250m$propLand_use[buf_250m$type %in% c("Hede", "Overdrev", "Eng")] <- "Open uncultivated land"
-buf_500m$propLand_use[buf_500m$type %in% c("Hede", "Overdrev", "Eng")] <- "Open uncultivated land"
-buf_1000m$propLand_use[buf_1000m$type %in% c("Hede", "Overdrev", "Eng")] <- "Open uncultivated land"
+buf_50m$propLand_use[buf_50m$type %in% c("Hede", "Hede_organic", "Overdrev", "Overdrev_organic", "Eng", "Eng_organic", "Strandeng", "Strandeng_organic")] <- "Open uncultivated land"
+buf_250m$propLand_use[buf_250m$type %in% c("Hede", "Hede_organic", "Overdrev", "Overdrev_organic", "Eng", "Eng_organic", "Strandeng", "Strandeng_organic")] <- "Open uncultivated land"
+buf_500m$propLand_use[buf_500m$type %in% c("Hede", "Hede_organic", "Overdrev", "Overdrev_organic", "Eng", "Eng_organic", "Strandeng", "Strandeng_organic")] <- "Open uncultivated land"
+buf_1000m$propLand_use[buf_1000m$type %in% c("Hede", "Hede_organic", "Overdrev", "Overdrev_organic", "Eng", "Eng_organic", "Strandeng", "Strandeng_organic")] <- "Open uncultivated land"
 
 #unspecified category
 buf_50m$propLand_use[buf_50m$type %in% "Andet"] <- "Unspecified land cover"
@@ -116,7 +124,7 @@ outputCast <- merge(outputCast, outputCast500, by = "routeID")
 outputCast <- merge(outputCast, outputCast1000, by = "routeID")
 
 #merge with oeko, urbangreen, and hedge cast outputs
-outputCast <- merge(outputCast, oekocast, by = "routeID")
+#outputCast <- merge(outputCast, oekocast, by = "routeID")
 outputCast <- merge(outputCast, hedgecast, by = "routeID")
 outputCast <- merge(outputCast, urbangreencast, by = "routeID")
 
